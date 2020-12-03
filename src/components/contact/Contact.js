633889ec5e92d51b05a31b2bdd593c005884e5c7
lamilekan263 <lamilekan263@gmail.com>
2020-12-03 14:54:33 +0100

    added new features

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -3,7 +3,7 @@ import './Contact.css'
 
 const Contact = () => {
     return (
-      <div className="contactPage">
+      <div className="contactPage my-5">
         <div className="container">
           <h1>
             <span>I'm always</span>
@@ -20,9 +20,20 @@ const Contact = () => {
             <br />
             <span>Project?</span>
             <br />
-            <span>Let's talk.</span>
+            <a href="mailto:lamilekan263@gmail.com">
+              <button className="mt-5">
+                {" "}
+                Let's Talk
+                <img
+                  className="ml-1 arrow"
+                  src="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iNzYiIGhlaWdodD0iMzAiIHZpZXdCb3g9IjAgMCA3NiAzMCIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KPHBhdGggZD0iTTAgMTVIMzMiIHN0cm9rZT0iYmxhY2siIHN0cm9rZS13aWR0aD0iMiIvPgo8cGF0aCBkPSJNNjYuMDEyNSAxMy43NUg1MVYxNi4yNUg2Ni4wMTI1VjIwTDcxIDE1TDY2LjAxMjUgMTBWMTMuNzVaIiBmaWxsPSJibGFjayIvPgo8Y2lyY2xlIGN4PSI0MS41IiBjeT0iMTQuNSIgcj0iMy41IiBmaWxsPSJibGFjayIvPgo8L3N2Zz4K"
+                  alt=""
+                />
+              </button>
+            </a>
           </h1>
         </div>
+        <div></div>
       </div>
     );
 }
